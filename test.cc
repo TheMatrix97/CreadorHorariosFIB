20d619342b541c9b3896ba462345684efbbadf6a
Marc Catrisse <marc@catrisse.net>
2016-07-07 10:32:58 -0700

    funcio llegir/escriure del dicc funciona!

---
+++ test.cc
@@ -0,0 +1,16 @@
+#include "diccionari.hh"
+using namespace std;
+
+
+
+int main(){
+	diccionari test;
+	cout << "num archius: " << endl;
+	int n;
+	cin >> n;
+	vector<string> vec(n);
+	for(int i = 0; i < n; i++) cin >> vec[i];
+	test.llegir(vec);
+	test.escriure();
+	
+}
